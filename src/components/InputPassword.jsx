3649fe08586ea269a1d157124bf448a3b183e125
AnhTuan <voanhtuan13321@gmail.com>
2023-11-22 16:50:46 +0700

    done

--- src/components/InputPassword.jsx
+++ src/components/InputPassword.jsx
@@ -1,12 +1,49 @@
-import React, { useId } from 'react'
+import React, { useId, useState } from 'react'
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
 
-export default function InputPassword({ value, onChange }) {
+/**
+ * <InputPassword label='label' name='name' value={text} onChange={event => setText(event.target.value)} />
+ *
+ * @param label
+ * @param name
+ * @param value
+ * @param onChange
+ */
+export default function InputPassword({ label, name, value, onChange }) {
   const id = useId()
+  const [isShowPassword, setShowPassword] = useState(false)
+
+  const onClickIconShowPassword = () => {
+    setShowPassword(!isShowPassword)
+  }
+
+  const PropsIcons = {
+    className: 'absolute top-2 right-2 cursor-pointer',
+    onClick: onClickIconShowPassword,
+  }
 
   return (
-    <div>
-      <label htmlFor={id}></label>
-      <input id={id} type='password' value={value} onChange={onChange} />
+    <div className='flex flex-col'>
+      <label htmlFor={id} className='py-2 text-main text-normal'>
+        {label}
+      </label>
+
+      <div className='relative'>
+        <input
+          id={id}
+          type={isShowPassword ? 'text' : 'password'}
+          name={name}
+          className='w-full p-2 rounded-md outline-none border border-normal text-main'
+          value={value}
+          onChange={onChange}
+        />
+
+        {isShowPassword ? (
+          <FaRegEye {...PropsIcons} />
+        ) : (
+          <FaRegEyeSlash {...PropsIcons} />
+        )}
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
       fontSize: {
         main: '12px',
+        big: '22px',
       },
     },
   },
