573546aeefe2739b90b3eb78b4c65e50774a0ae9
AnhTuan <voanhtuan13321@gmail.com>
2023-11-22 16:16:30 +0700

    ttttt

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,12 +1,29 @@
 import React, { useId } from 'react'
 
-export default function InputPassword({ value, onChange }) {
+/**
+ * <InputText label='label' name='name' value={text} onChange={event => setText(event.target.value)} />
+ *
+ * @param label
+ * @param name
+ * @param value
+ * @param onChange
+ */
+export default function InputPassword({ label, name, value, onChange }) {
   const id = useId()
 
   return (
-    <div>
-      <label htmlFor={id}></label>
-      <input id={id} type='text' value={value} onChange={onChange} />
+    <div className='flex flex-col'>
+      <label htmlFor={id} className='py-2'>
+        {label}
+      </label>
+      <input
+        id={id}
+        type='text'
+        name={name}
+        className='p-2 rounded-md outline-none border'
+        value={value}
+        onChange={onChange}
+      />
     </div>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import InputText from '../components/InputText'
 
 export default function Home() {
-  return <div>Home</div>
+  const [text, setText] = React.useState('')
+
+  return (
+    <div className='mt-5'>
+      Home{' '}
+      <InputText
+        label='label'
+        name='name'
+        value={text}
+        onChange={event => setText(event.target.value)}
+      />
+    </div>
+  )
 }
