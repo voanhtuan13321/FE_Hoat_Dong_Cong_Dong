462b65894f22298e321fe3dd9abb1f93e037129b
AnhTuan <voanhtuan13321@gmail.com>
2023-11-23 10:02:15 +0700

    ...

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Button from '../components/Button'
-import InputPassword from '../components/InputPassword'
 
 export default function Home() {
   return <div>Home</div>
