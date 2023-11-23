34583cb8e58cffea1d7cf57396b873e81348ad20
AnhTuan <voanhtuan13321@gmail.com>
2023-11-23 10:15:30 +0700

    modify button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { MdEdit, MdDelete } from 'react-icons/md'
+import { MdEdit, MdDelete, MdAdd } from 'react-icons/md'
 
 const icon = {
   edit: <MdEdit />,
   delete: <MdDelete />,
+  add: <MdAdd />,
 }
 
 /**
  * <Button type='primary' label='Tìm kiếm' onClick={event => alert('click')} />
  *
- * @param type loại button (primary, outline, edit, delete)
+ * @param type loại button (primary, outline, edit, delete, add)
  * @param label chữ bên trong button
  * @param onClick function xử lí khi click vào button
  */
@@ -22,22 +23,24 @@ export default function Button({ type, label, onClick }) {
         return 'bg-yellow-400 text-white'
       case 'delete':
         return 'bg-red-500 text-white'
+      case 'add':
+        return 'bg-primary text-white'
       default:
         return 'bg-primary text-white'
     }
   }
 
-  const isEditOrDelete = () => ['edit', 'delete'].includes(type)
+  const isShowIcon = () => ['edit', 'delete', 'add'].includes(type)
 
   return (
     <button
       className={`px-4 py-2 rounded-md hover:opacity-80 uppercase text-main relative
-        ${renClassName()} ${isEditOrDelete() && 'pl-8'}`}
+        ${renClassName()} ${isShowIcon() && 'pl-8'}`}
       onClick={onClick}
     >
       {label}
       <div className='absolute top-0 bottom-0 left-3 flex items-center'>
-        {isEditOrDelete() && icon[type]}
+        {isShowIcon() && icon[type]}
       </div>
     </button>
   )
