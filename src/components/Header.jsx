5b56578f06012c2b8baf0c45f2a9c8ca52f471a1
Thang <thang661997@gmail.com>
2023-11-27 15:57:54 +0700

    upload header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Logo from '../assets/images/Logo.png'
 
 export default function Header() {
   return (
-    <div className='container mx-40 py-3 flex flex-row items-center gap-4'>
+    <div className='w-full px-40 py-3 flex flex-row items-center gap-4'>
       <img src={Logo} alt='Logo' />
       <p className='text-primary font-bold font-big uppercase'>
         HỆ THỐNG THÔNG TIN SINH VIÊN
