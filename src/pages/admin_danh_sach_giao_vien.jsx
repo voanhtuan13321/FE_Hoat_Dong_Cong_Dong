8611e6cc4f3bf69b185e4061283122f942351ba9
AnhTuan <voanhtuan13321@gmail.com>
2023-11-28 21:37:47 +0700

    done

--- src/pages/admin_danh_sach_giao_vien.jsx
+++ src/pages/admin_danh_sach_giao_vien.jsx
@@ -1,28 +1,72 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from '../components/Table'
+import Title from '../components/Title'
+import Button from '../components/Button'
+import ItemRowTableDanhSachGiaoVienAdmin from '../components/ItemRowTableDanhSachGiaoVienAdmin'
+import ItemRowTableDanhSachGiaoVienAdminAdd from '../components/ItemRowTableDanhSachGiaoVienAdminAdd'
 
 const dataTable = {
   header: [
-    { className: 'w-1/12', title: 'stt' },
-    { className: 'w-3/12', title: 'mã giáo viên' },
-    { className: 'w-3/12', title: 'họ và tên' },
-    { className: 'w-2/12', title: 'đổi mật khẩu' },
-    { className: 'w-2/12', title: 'khoá tài khoản' },
+    { className: 'w-5%', title: 'stt' },
+    { className: 'w-20%', title: 'mã giáo viên' },
+    { className: '', title: 'họ và tên' },
+    { className: 'w-20%', title: 'đổi mật khẩu' },
+    { className: 'w-20%', title: 'khoá tài khoản' },
   ],
   value: [
-    { stt: 1, email: 'test@example.com' },
-    { stt: 1, email: 'test@example.comqwerqwe' },
+    { maGiaoVien: 'gv001', hoVaTen: 'vo anh tuan 1', disabled: true },
+    { maGiaoVien: 'gv002', hoVaTen: 'vo anh tuan 2', disabled: false },
+    { maGiaoVien: 'gv003', hoVaTen: 'vo anh tuan 3', disabled: true },
+    { maGiaoVien: 'gv004', hoVaTen: 'vo anh tuan 4', disabled: false },
   ],
 }
 
 export default function AdminDanhSachGiaoVien() {
+  const [listGiaoVien, setListGiaoVien] = useState([])
+  const [isShowAddNew, setShowAddNew] = useState(false)
+
+  useEffect(() => {
+    fetchListGiaoVien()
+  }, [])
+
+  const fetchListGiaoVien = () => {
+    setListGiaoVien(dataTable.value)
+  }
+
+  const onClickThem = () => {
+    setShowAddNew(true)
+  }
+
+  const renderBodyTable = () => {
+    let arrJsx = listGiaoVien.map((dt, index) => {
+      return (
+        <ItemRowTableDanhSachGiaoVienAdmin key={index} stt={index} data={dt} />
+      )
+    })
+
+    isShowAddNew &&
+      (arrJsx = [
+        ...arrJsx,
+        <ItemRowTableDanhSachGiaoVienAdminAdd
+          key={-1}
+          setShowAddNew={setShowAddNew}
+        />,
+      ])
+
+    return arrJsx
+  }
+
   return (
     <div className='container mx-auto'>
-      <div>Title</div>
+      <Title title='danh sách giao viên' />
       <div>
-        <div>button</div>
+        <div className='py-2 text-end'>
+          {!isShowAddNew && (
+            <Button type='add' label='thêm' onClick={onClickThem} />
+          )}
+        </div>
         <div>
-          <Table data={dataTable} />
+          <Table header={dataTable.header}>{renderBodyTable()}</Table>
         </div>
       </div>
     </div>
