1e8d5eddd79d7c06fc2475cbdfcb9a76c940f8c9
minhtri2510 <minhtricgv@gmail.com>
2023-11-29 14:19:21 +0700

    fix comment

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Title from '../components/Title'
 
 export default function Home() {
   return (
     <>
-      <Title title={'Trang chủ'}></Title>
       <div>Home</div>
     </>
   )
