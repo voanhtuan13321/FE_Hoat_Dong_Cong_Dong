4ef758aa6c397857d55ad15cb57c88ff1eba0f08
minhtri2510 <minhtricgv@gmail.com>
2023-11-29 14:43:03 +0700

    fix comment

--- src/pages/ket_qua_phuc_vu_cong_dong.jsx
+++ src/pages/ket_qua_phuc_vu_cong_dong.jsx
@@ -1,45 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from '../components/Title'
 import Table from '../components/Table'
 
+const dataTable = {
+  header: [
+    { className: 'w-5%', title: 'stt' },
+    { className: 'w-15%', title: 'Năm' },
+    { className: '', title: 'Tên hoạt động' },
+    { className: 'w-10%', title: 'Điểm' },
+    { className: 'w-20%', title: 'Ghi chú' },
+  ],
+  value: [
+    { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
+    { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
+    { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
+    { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
+    { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
+    { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
+  ],
+}
+
 export default function KetQuaPhucVuCongDong() {
-  const dataTable = {
-    header: [
-      { className: 'w-1/12', title: 'stt' },
-      { className: 'w-2/12', title: 'Năm' },
-      { className: 'w-5/12', title: 'Tên hoạt động' },
-      { className: 'w-2/12', title: 'Điểm' },
-      { className: 'w-2/12', title: 'Ghi chú' },
-    ],
-    value: [
-      { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
-      { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
-      { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
-      { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
-      { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
-      { stt: 1, year: '2012-2013', name: 'Hiển máu', point: '10', note: '' },
-    ],
-  }
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    setData(dataTable.value)
+  }, [])
+
   const renderBodyTable = () => {
-    return dataTable.value.map((dt, index) => {
+    let arrJsx = data.map((dt, index) => {
       return (
         <tr key={index}>
-          <td className='border border-primary p-1 text-center'>{dt.stt}</td>
-          <td className='border border-primary p-1 text-center'>{dt.year}</td>
-          <td className='border border-primary p-1 text-center'>{dt.name}</td>
-          <td className='border border-primary p-1 text-center'>{dt.point}</td>
-          <td className='border border-primary p-1'>{dt.note}</td>
+          <td className='border border-primary p-1 text-center text-main'>
+            {dt.stt}
+          </td>
+          <td className='border border-primary p-1 text-center  text-main'>
+            {dt.year}
+          </td>
+          <td className='border border-primary p-1 text-center  text-main'>
+            {dt.name}
+          </td>
+          <td className='border border-primary p-1 text-center  text-main'>
+            {dt.point}
+          </td>
+          <td className='border border-primary p-1  text-main'>{dt.note}</td>
         </tr>
       )
     })
+    return arrJsx
   }
 
   return (
     <>
       <div className='container justify-center m-auto p-2'>
-        <Title title={'Kết quả phục vụ cộng đồng'} ></Title>
+        <Title title={'Kết quả phục vụ cộng đồng'}></Title>
         <div className='my-2'>
-          <Table data={dataTable}>{renderBodyTable()}</Table>
+          <Table header={dataTable.header}>{renderBodyTable()}</Table>
         </div>
       </div>
     </>
