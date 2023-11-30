3a9ae01b163770094ae8540406ebb2e3a8deaf9c
minhtri2510 <minhtricgv@gmail.com>
2023-11-30 14:15:24 +0700

    TriDM5_Page_HoatDongCongDongCuaTruongAdmin

--- src/pages/admin_hoat_dong_cong_dong_cua_truong.jsx
+++ src/pages/admin_hoat_dong_cong_dong_cua_truong.jsx
@@ -1,5 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Title from '../components/Title'
+import Table from '../components/Table'
+import Button from '../components/Button'
+
+const dataTable = {
+  header: [
+    { className: 'w-5%', title: 'STT' },
+    { className: 'w-20%', title: 'Mã sinh viên' },
+    { className: '', title: 'Họ và tên' },
+    { className: 'w-20%', title: 'Lớp' },
+    { className: 'w-20%', title: 'Điểm' },
+  ],
+  value: [
+    {
+      maSinhVien: 'gv001',
+      hoVaTen: 'vo anh tuan 1',
+      lop: '18CNTT',
+      diem: '40',
+    },
+    {
+      maSinhVien: 'gv001',
+      hoVaTen: 'vo anh tuan 1',
+      lop: '18CNTT',
+      diem: '40',
+    },
+    {
+      maSinhVien: 'gv001',
+      hoVaTen: 'vo anh tuan 1',
+      lop: '18CNTT',
+      diem: '40',
+    },
+    {
+      maSinhVien: 'gv001',
+      hoVaTen: 'vo anh tuan 1',
+      lop: '18CNTT',
+      diem: '40',
+    },
+    {
+      maSinhVien: 'gv001',
+      hoVaTen: 'vo anh tuan 1',
+      lop: '18CNTT',
+      diem: '40',
+    },
+    {
+      maSinhVien: 'gv001',
+      hoVaTen: 'vo anh tuan 1',
+      lop: '18CNTT',
+      diem: '40',
+    },
+    {
+      maSinhVien: 'gv001',
+      hoVaTen: 'vo anh tuan 1',
+      lop: '18CNTT',
+      diem: '40',
+    },
+    {
+      maSinhVien: 'gv001',
+      hoVaTen: 'vo anh tuan 1',
+      lop: '18CNTT',
+      diem: '40',
+    },
+  ],
+}
 
 export default function AdminHoatDongCongDongCuaTruong() {
-  return <div>AdminHoatDongCongDongCuaTruong</div>
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    setData(dataTable.value)
+  }, [])
+
+  const renderBodyTable = () => {
+    let arrJsx = data.map((dt, index) => {
+      return (
+        <tr>
+          <td className='border border-primary p-1 text-center text-main'>
+            {index + 1}
+          </td>
+          <td className='border border-primary p-1 text-center text-main'>
+            {dt.maSinhVien}
+          </td>
+          <td className='border border-primary p-1 text-center text-main'>
+            {dt.hoVaTen}
+          </td>
+          <td className='border border-primary p-1 text-center text-main'>
+            {dt.lop}
+          </td>
+          <td className='border border-primary p-1 text-center text-main'>
+            {dt.diem}
+          </td>
+        </tr>
+      )
+    })
+    return arrJsx
+  }
+
+  return (
+    <>
+      <div className='container mx-auto'>
+        <div className='p-2'>
+          <Title title='Danh sách sinh viên của trường' />
+        </div>
+        <div className='p-2'>
+          <div className='text-end pb-2'>
+            <Button type={''} label={'Xuất file'} />
+          </div>
+          <Table header={dataTable.header}>{renderBodyTable()}</Table>
+        </div>
+      </div>
+    </>
+  )
 }
