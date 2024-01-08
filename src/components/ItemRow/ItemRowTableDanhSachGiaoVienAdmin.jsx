d55497a0c7a7e831b54bb442f976a5868e6d9fb0
manhle117 <leducmanh113@gmail.com>
2024-01-08 23:44:21 +0700

    new commit dsgv

--- src/components/ItemRow/ItemRowTableDanhSachGiaoVienAdmin.jsx
+++ src/components/ItemRow/ItemRowTableDanhSachGiaoVienAdmin.jsx
@@ -1,48 +1,63 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Swal from 'sweetalert2'
 import Button from '../Button'
 import InputCheckbox from '../Input/InputCheckbox'
 import DialogCreateUserTeacher from '../DialogCustom/DialogCreateUserTeacher'
+import DialogChangePassword from '../DialogCustom/DialogChangePassword'
+import { useNavigate } from 'react-router-dom'
+import { STATUS_USER, callApiUpdateUserStatus, handleError } from '../../utils'
 
 export default function ItemRowTableDanhSachGiaoVienAdmin({ stt, data }) {
+  const [isShowDialog, setShowDialog] = useState(false)
+  const [user, setUser] = useState(data)
+  const navigate = useNavigate()
 
-  const onClickDoiMatKhau = () => {
-    Swal.fire({
-      title: 'Nhập mật khẩu mới',
-      input: 'text',
-      showCancelButton: true,
-      cancelButtonText: 'huỷ',
-      confirmButtonText: 'Xác nhận',
-      showLoaderOnConfirm: true,
-      preConfirm: async valuePassword => {
-        if (!valuePassword) {
-          return Swal.showValidationMessage('bạn chưa nhập mật khẩu')
-        }
-      },
-    }).then(result => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          icon: 'success',
-          title: `Thay đổi mật khẩu thành công!`,
-          showConfirmButton: false,
-          timer: 1500,
-        })
-      }
-    })
+  useEffect(() => {
+    setUser(data)
+  }, [data])
+
+  const changeStatusAccountUser = async () => {
+    try {
+      const dataRequest = { userId: user.id, status: revertStatus(user.status) }
+      const data = await callApiUpdateUserStatus(dataRequest)
+      // console.log(data)
+      setUser(data)
+    } catch (error) {
+      console.error(error)
+      handleError(error, navigate)
+    }
   }
 
+  const revertStatus = status => {
+    return status === STATUS_USER.ACCOUNT_LOCKED
+      ? STATUS_USER.ACCOUNT_UNLOCK
+      : STATUS_USER.ACCOUNT_LOCKED
+  }
   return (
     <tr>
-      <td className='border border-primary p-1 text-center'>{stt +1 }</td>
+      <td className='border border-primary p-1 text-center'>
+        {stt + 1}{' '}
+        <DialogChangePassword
+          userId={user.id}
+          isShowDialog={isShowDialog}
+          setShowDialog={setShowDialog}
+        />
+      </td>
       <td className='border border-primary p-1 text-center'>
         {data.teacherId}
       </td>
-      <td className='border border-primary p-1'>{data.firstName + " " + data.lastName}</td>
-      <td className='border border-primary p-1 text-center'>
-        <Button type='edit' label='sửa' onClick={onClickDoiMatKhau} />
+      <td className='border border-primary p-1'>
+        {data.firstName + ' ' + data.lastName}
       </td>
       <td className='border border-primary p-1 text-center'>
-        <InputCheckbox value={data.disabled} onChange={() => {}} />
+        <Button type='edit' label='sửa' onClick={() => setShowDialog(true)} />
+      </td>
+      <td className='border border-primary p-1 text-center'>
+        <InputCheckbox
+          value={user.status === STATUS_USER.ACCOUNT_LOCKED}
+          onClick={changeStatusAccountUser}
+          onChange={() => {}}
+        />
       </td>
     </tr>
   )
